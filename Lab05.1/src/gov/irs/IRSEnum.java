44717650433129578c846d26faa23740d1cc5a7d
John-DanielVan <JohnV@tlgcohort.com>
2024-06-10 14:28:18 -0500

    IRSEnum singleton with IRS interface and static factory method for the sole instance.

--- Lab05.1/src/gov/irs/IRSEnum.java
+++ Lab05.1/src/gov/irs/IRSEnum.java
@@ -11,11 +11,13 @@ package gov.irs;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public enum IRSEnum {
+enum IRSEnum implements IRS {
     INSTANCE;
 
     // BUSINESS CODE
-    private Collection<TaxPayer> payers = new ArrayList<>();
+    private final Collection<TaxPayer> payers = new ArrayList<>();
+
+
 
     public void collectTaxes() {
         for (TaxPayer payer : payers) {
@@ -28,25 +30,5 @@ public enum IRSEnum {
     }
 
 
-    // constructor - just so we can see when INSTANCE is created
-    IRSEnum() {
-        System.out.println("--IRSEnum ctor: the instance has been created");
-    }
 
-    // not needed - just makes the enum look more like a regular singleton
-    public static IRSEnum getInstance() {
-        return INSTANCE;
-    }
-
-    // static initializer block - just so we can see when IRSEnum.class is loaded
-    // a class load triggers creation of an enum's instances (only one in this case - INSTANCE)
-    static {
-        System.out.println("--IRSEnum class loaded");
-    }
-
-    // superfluous static method that shouldn't be here
-    // if it gets called, IRSEnum.class is loaded and INSTANCE is created (prematurely)
-    public static void touch() {
-        // no-op
-    }
 }
